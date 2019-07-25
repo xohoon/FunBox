003a65b882271ddc6d408112c8ecc5d94cd2ddd5
OvollovO <40098076+psk876@users.noreply.github.com>
2019-07-25 17:20:33 +0900

    박신규 Main 페이지 VO 랑 ACtion 이랑 DAO

--- src/net/member/action/MainAction.java
+++ src/net/member/action/MainAction.java
@@ -3,19 +3,33 @@ package net.member.action;
 
 
 
+import java.util.ArrayList;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import net.common.action.Action;
 import net.common.action.ActionForward;
+import net.company.dao.CompanyDAO;
+import net.page.dto.MainPageDateOfOpenVO;
+import net.page.dto.MainPageDeadLineVO;
 
 ///////////////////////유정 추가///////////////////////
 public class MainAction implements Action {
-	@SuppressWarnings("unchecked")
 	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) 
 			throws Exception {
-		
 		ActionForward forward = new ActionForward();
+		
+		CompanyDAO companyDAO = new CompanyDAO();
+		ArrayList<MainPageDateOfOpenVO> mainPageDateOfOpenVOs = companyDAO.getCompanyDateOfOpen();
+		companyDAO = new CompanyDAO();
+		ArrayList<MainPageDeadLineVO> mainPageDeadLineVOs = companyDAO.getCompanyDeadLine();
+		
+		
+		
+		request.setAttribute("mainPageDateOfOpenVOs", mainPageDateOfOpenVOs);
+		request.setAttribute("mainPageDeadLineVOs", mainPageDeadLineVOs);
+		
 		forward.setRedirect(false);
 		forward.setPath("./index.jsp");
 		return forward;
