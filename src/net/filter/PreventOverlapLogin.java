d1d51faa46aa63da575c47cdec8d4d109658e665
OvollovO <40098076+psk876@users.noreply.github.com>
2019-08-13 14:19:01 +0900

    중복 로그인 TEST

---
+++ src/net/filter/PreventOverlapLogin.java
@@ -0,0 +1,39 @@
+package net.filter;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpSession;
+
+import net.member.util.EgovHttpSessionBindingListener;
+
+public class PreventOverlapLogin implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+//		HttpSession session = request.get
+//		request.getSession().setAttribute("TAATLoginId", request.getParameter("id"));
+//		EgovHttpSessionBindingListener httpSessionBindingListener = new EgovHttpSessionBindingListener();
+//		
+//		
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
